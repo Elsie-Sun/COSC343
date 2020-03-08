20ab1934d6cc939f4ef69c08658e936522149c51
Kimetsu <49518411+Elsie-Sun@users.noreply.github.com>
2020-03-08 16:22:40 +1300

    Update move.py

--- Assignment1/move.py
+++ Assignment1/move.py
@@ -1,34 +1,60 @@
 #!/usr/bin/env python3
-from ev3dev2 . motor import *
+from ev3dev2.motor import *
 from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor.lego import GyroSensor
 from ev3dev2.sound import Sound
 from time import sleep
 
+## sensors
 cl = ColorSensor()
 cl.mode = 'COL-COLOR'
 
-# Attach large motors to ports B and C
+sound = Sound()
+gy = GyroSensor() # not used now
+
+## controls robot
+drive = MoveTank(OUTPUT_B, OUTPUT_C)
 mB = LargeMotor('outB')
 mC = LargeMotor('outC')
 
+# count how many black tiles the robot passed
 count = 0
-sound = Sound()
-
-mB.run_forever(speed_sp=150)
-mC.run_forever(speed_sp=150)
-
-while cl.color == 1:
-    count += 1
-    if count == 1:
-        mB.run_to_rel_pos(position_sp=340, speed_sp=200, stop_action="brake")
-        sound.speak(count)
-
-    sound.speak(count)
-
-    mB.wait_while('running')
-    mC.wait_while('running')
 
 
+while True:
+    if count < 1:
+        drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), 0.6)
 
+    # detects a black tile
+    if cl.color == 1:
+        count += 1
+        if count == 1:
+            mB.run_to_rel_pos(position_sp=340, speed_sp=360, stop_action="brake")
+            sound.speak(count)
+            drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), 0.5)
+        elif 1 < count < 7:
+            sound.speak(count)
+            drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), 1.2)
+        elif count == 7:
+            mB.run_to_rel_pos(position_sp=340, speed_sp=360, stop_action="brake")
+            sound.speak(count)
+            drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), 2.1)
+        elif 7 < count < 13:
+            sound.speak(count)
+            drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), 2.1)
+        elif count == 13:
+            mC.run_to_rel_pos(position_sp=340, speed_sp=360, stop_action="brake")
+            sound.speak(count)
 
+    # do not detect a black tile
+    # move back and correct its position(not complete)
+    elif cl.color != 1:
+        if count < 7:
+            count -= 1
+            drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), -1.2)
 
+        elif 7 < count < 13:
+            count -= 1
+            drive.on_for_rotations(SpeedPercent(20), SpeedPercent(20), -2.1)
+
+sleep(2)
